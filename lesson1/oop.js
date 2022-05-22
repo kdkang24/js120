323d958003e538ff136c7030b5bc9ec5bb072fc3
Kevin Kang <kdkang24@gmail.com>
2022-05-22 03:45:37 +0000

    intro to OOP

---
+++ lesson1/oop.js
@@ -0,0 +1,25 @@
+let raceCar = {
+  make: 'BMW',
+  fuelLevel: 0.5,
+  engineOn: false,
+
+  startEngine() {
+    this.engineOn = true;
+  },
+
+  drive() {
+    this.fuelLevel -= 0.1;
+  },
+
+  stopEngine() {
+    this.engineOn = false;
+  },
+
+  refuel(percent) {
+    if ((this.fuelLevel + (percent / 100)) <= 1) {
+      this.fuelLevel += (percent / 100);
+    } else {
+      this.fuelLevel = 1;
+    }
+  },
+};

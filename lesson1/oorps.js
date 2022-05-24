58e2c8df92a500cbead0e13dfd9d0a31f3a2076c
Kevin Kang <kdkang24@gmail.com>
2022-05-24 04:37:36 +0000

    added move history and computer adjustment

--- lesson1/oorps.js
+++ lesson1/oorps.js
@@ -1,3 +1,6 @@
+/* eslint-disable max-len */
+/* eslint-disable max-statements */
+/* eslint-disable max-lines-per-function */
 const readline = require('readline-sync');
 
 function createPlayer() {
@@ -11,11 +14,13 @@ function createPlayer() {
 function createComputer() {
   let playerObject = createPlayer();
 
+  //Changed `choices` into a property of `computerObject` so that it can be modified
   let computerObject = {
+    choices: ['rock', 'paper', 'scissors'],
+
     choose() {
-      const choices = ['rock', 'paper', 'scissors'];
-      let randomIndex = Math.floor(Math.random() * choices.length);
-      this.move = choices[randomIndex];
+      let randomIndex = Math.floor(Math.random() * this.choices.length);
+      this.move = this.choices[randomIndex];
     }
   };
 
@@ -31,9 +36,9 @@ function createHuman() {
       let choice;
 
       while (true) {
-        console.log('Please choose rock, paper, or scissors:');
+        console.log('Please choose Rock, Paper, or Scissors:');
         choice = readline.question();
-        if (['rock', 'paper', 'scissors'].includes(choice)) break;
+        if (['rock', 'paper', 'scissors'].includes(choice.toLowerCase())) break;
         console.log('Sorry, invalid choice.');
       }
 
@@ -47,50 +52,136 @@ function createHuman() {
 const RPSGame = {
   human: createHuman(),
   computer: createComputer(),
+  //Array to keep track of move history and outcomes
+  moveHistory: [],
 
-  displayWelcomeMessage() {
-    console.log('Welcome To Rock, Paper, Scissors!');
+  adjustComputerChoice() {
+    //Readjust choice weights after each `ADJUSTMENT_INTERVAL` number of games
+    //Changed var `choices` in computerObject into a property (Completed above)
+    //`choices` array will have additional choices added every `ADJUSTMENT_INTERVAL`
+    //The move with the highest cumulative loss rate at every `ADJUSTMENT_INTERVAL`
+    //will not be added to `choices` for that iteration
+
+    //Potential issues
+    //`choices` array may grow excessively long if many games are played
+    const ADJUSTMENT_INTERVAL = 5;
+    let historyLength = this.moveHistory.length;
+    if (historyLength > 0 && historyLength % ADJUSTMENT_INTERVAL === 0) {
+      let lossRates = this.calculateLossRates();
+      let choices = ['rock', 'paper', 'scissors'];
+
+      //Sort loss rates object to get highest loss rate move at index 0
+      lossRates = Object.entries(lossRates).sort((a, b) => b[1] - a[1]);
+
+      let biggestLoser = choices.indexOf(lossRates[0][0]);
+      choices.splice(biggestLoser, 1);
+      this.computer.choices = this.computer.choices.concat(choices);
+    }
+    console.log(this.computer.choices);
   },
 
-  displayWinner() {
+  calculateLossRates() {
+    let rockLossRate = 0;
+    let paperLossRate = 0;
+    let scissorsLossRate = 0;
+    let totalGames = this.moveHistory.length;
+
+    this.moveHistory.forEach(move => {
+      if (move.Computer === 'ROCK' && move.Winner === 'You') {
+        rockLossRate += 1;
+      } else if (move.Computer === 'PAPER' && move.Winner === 'You') {
+        paperLossRate += 1;
+      } else if (move.Computer === 'SCISSORS' && move.Winner === 'You') {
+        scissorsLossRate += 1;
+      }
+    });
+
+    rockLossRate /= totalGames;
+    paperLossRate /= totalGames;
+    scissorsLossRate /= totalGames;
+
+    return {
+      rock: rockLossRate,
+      paper: paperLossRate,
+      scissors: scissorsLossRate,
+    };
+  },
+
+  checkWinner() {
     let humanMove = this.human.move;
     let computerMove = this.computer.move;
+    //moveInfo object to be added to moveHistory after each game
+    let moveInfo = {
+      You: humanMove.toUpperCase(),
+      Computer: computerMove.toUpperCase(),
+    };
 
-    console.log(`You chose: ${this.human.move}`);
-    console.log(`The computer chose: ${this.computer.move}`);
+    console.log('---------------------------------');
+    console.log(`You chose: ${humanMove.toUpperCase()}`);
+    console.log(`The computer chose: ${computerMove.toUpperCase()}`);
+    console.log('---------------------------------');
 
     if ((humanMove === 'rock' && computerMove === 'scissors') ||
         (humanMove === 'paper' && computerMove === 'rock') ||
         (humanMove === 'scissors' && computerMove === 'paper')) {
       console.log('You win!');
       this.human.score += 1;
+      moveInfo.Winner = 'You';
+
     } else if ((humanMove === 'rock' && computerMove === 'paper') ||
                (humanMove === 'paper' && computerMove === 'scissors') ||
                (humanMove === 'scissors' && computerMove === 'rock')) {
       console.log('Computer wins!');
       this.computer.score += 1;
+      moveInfo.Winner = 'Computer';
     } else {
-      console.log("It's a tie");
+      console.log("It's a tie.");
+      moveInfo.Winner = 'Tie';
     }
-    console.log(`The current score: You - ${this.human.score} and Computer - ${this.computer.score}`);
+    this.moveHistory.push(moveInfo);
+    console.log(`The new score: You - ${this.human.score} and Computer - ${this.computer.score}`);
   },
 
-  //New method to check if either player has reached 5 points
-  checkForGrandWinner() {
+  //New method to check if either player has enough wins to be grand champion
+  checkForGrandChampion() {
     const GRAND_CHAMPION_WINCOUNT = 5;
     if (this.human.score === GRAND_CHAMPION_WINCOUNT) {
+      console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
       console.log(`Congratulations! You have beaten the computer ${GRAND_CHAMPION_WINCOUNT} times! You are the GRAND CHAMPION!`);
+      console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
       this.human.score = 0;
       this.computer.score = 0;
     } else if (this.computer.score === GRAND_CHAMPION_WINCOUNT) {
+      console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
       console.log(`The computer has beaten you ${GRAND_CHAMPION_WINCOUNT} times! The computer is the GRAND CHAMPION!`);
+      console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
       this.human.score = 0;
       this.computer.score = 0;
     }
   },
 
+  //New method to display game info (current score and move history)
+  displayGameInfo() {
+    console.log(`The current score: You - ${this.human.score} and Computer - ${this.computer.score}`);
+    if (this.moveHistory.length > 0) {
+      let last = this.moveHistory.length - 1;
+      console.log('============================================');
+      console.log(`Details from previous game:`);
+      console.log(`You picked ${this.moveHistory[last]['You']}, the Computer picked ${this.moveHistory[last]['Computer']}.`);
+      console.log('============================================');
+    }
+  },
+
+  displayWelcomeMessage() {
+    console.log('=================================');
+    console.log('Welcome To Rock, Paper, Scissors!');
+    console.log('=================================');
+  },
+
   displayGoodbyeMessage() {
+    console.log('==================================================');
     console.log('Thanks for playing Rock, Paper, Scissors. Goodbye!');
+    console.log('==================================================');
   },
 
   playAgain() {
@@ -102,11 +193,19 @@ const RPSGame = {
   play() {
     this.displayWelcomeMessage();
     while (true) {
+      this.displayGameInfo();
       this.human.choose();
       this.computer.choose();
-      this.displayWinner();
-      this.checkForGrandWinner();
-      if (!this.playAgain()) break;
+      this.checkWinner();
+      this.checkForGrandChampion();
+      this.adjustComputerChoice();
+      //Added some logic to clear console between rounds
+      if (!this.playAgain()) {
+        console.clear();
+        break;
+      } else {
+        console.clear();
+      }
     }
     this.displayGoodbyeMessage();
   },
--- lesson1/practice.js
+++ lesson1/practice.js
@@ -42,13 +42,59 @@ function createBook(title, author, read = false) {
   return book;
 }
 
-let book1 = createBook('Mythos', 'Stephen Fry');
-let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris');
-let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse');
+// let book1 = createBook('Mythos', 'Stephen Fry');
+// let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris');
+// let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse');
 
-console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
-book1.readBook();
-console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
-book2.getDescription();  // "Me Talk Pretty One Day was written by David Sedaris."
-book3.getDescription();  // "Aunts aren't Gentlemen was written by PG Wodehouse"
+// console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
+// book1.readBook();
+// console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
+// book2.getDescription();  // "Me Talk Pretty One Day was written by David Sedaris."
+// book3.getDescription();  // "Aunts aren't Gentlemen was written by PG Wodehouse"
 
+const readline = require('readline-sync');
+
+function createPlayer() {
+  return {
+    move: null,
+    //Add score property to generic player object
+    score: 0,
+  };
+}
+
+function createComputer() {
+  let playerObject = createPlayer();
+
+  let computerObject = {
+    choices: ['rock', 'paper', 'scissors'],
+
+    choose() {
+      let randomIndex = Math.floor(Math.random() * this.choices.length);
+      this.move = this.choices[randomIndex];
+    }
+  };
+
+  return Object.assign(playerObject, computerObject);
+}
+
+
+function createHuman() {
+  let playerObject = createPlayer();
+
+  let humanObject = {
+    choose() {
+      let choice;
+
+      while (true) {
+        console.log('Please choose Rock, Paper, or Scissors:');
+        choice = readline.question();
+        if (['rock', 'paper', 'scissors'].includes(choice.toLowerCase())) break;
+        console.log('Sorry, invalid choice.');
+      }
+
+      this.move = choice;
+    },
+  };
+
+  return Object.assign(playerObject, humanObject);
+}
